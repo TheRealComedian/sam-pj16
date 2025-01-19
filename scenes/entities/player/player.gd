77eb4a57174c64b869688a1c7c7ecde7e83541e8
BlueGravez <BlueGravez@users.noreply.github.com>
2025-01-19 14:26:57 -0500

    changed: camera positioning and sizing screen properties (windowed, half resolution) player.gd movement
    added:
    controller movement support
    current sprite sheets
    player.gd momentum system (for interlacing position)

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,7 +1,9 @@
 class_name Player extends CharacterBody2D
 
 @export var input_disabled: bool = false
-@export var movement_speed: int = 600
+@export var movement_speed: int = 300
+const momentum = 1200
+const friction = 600
 @onready var cursor: AnimatedSprite2D = $Cursor
 @onready var cursor_ray: RayCast2D = $CursorFacing
 
@@ -12,17 +14,17 @@ var angle_to_cursor: float:
 		return self.get_angle_to(get_global_mouse_position())
 
 func _ready():
-	if Global.player: 
+	if Global.player:
 		self.queue_free()
 		return 
 	Global.player = self
 
-func _physics_process(_delta):
+func _physics_process(delta: float) -> void:
 	cursor.global_position = get_global_mouse_position()
 	# Placeholder visual to show what direction a weapon would be facing
 	cursor_ray.rotation = angle_to_cursor
 	
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if !input_disabled: 
-		var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-		self.velocity = input_direction * movement_speed
-		move_and_slide()
+		self.velocity = velocity.move_toward(input_direction * movement_speed, momentum * delta) 
+	move_and_slide()
