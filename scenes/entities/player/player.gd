705730e4ceec7b1647af03ec055fd47da1ae8e67
Andres Graciano <andres27l97@gmail.com>
2025-01-19 15:44:21 -0600

    branch creation

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -23,7 +23,7 @@ func _physics_process(delta: float) -> void:
 	cursor.global_position = get_global_mouse_position()
 	# Placeholder visual to show what direction a weapon would be facing
 	cursor_ray.rotation = angle_to_cursor
-	
+	#la li lu le lo
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if !input_disabled: 
 		self.velocity = velocity.move_toward(input_direction * movement_speed, momentum * delta) 
