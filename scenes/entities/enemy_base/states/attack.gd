a5ef51ee46561efb73cdfa19012e6a1ede3ce13d
TheRealComedian <zacharylb3@gmail.com>
2025-01-22 18:59:46 -0500

    added cutoff for animations when leaving attack state

--- scenes/entities/enemy_base/states/attack.gd
+++ scenes/entities/enemy_base/states/attack.gd
@@ -6,3 +6,7 @@ func enter():
 	#TODO: add 'windup' period where enemy projects an attack before executing giving change to dodge
 	await (owner as Enemy).weapon.attack()
 	Transitioned.emit(self, 'Idle')
+
+func exit():
+	if owner.weapon.current_animation_tween:
+		owner.weapon.current_animation_tween.kill()
--- scenes/weapons/sword.gd
+++ scenes/weapons/sword.gd
@@ -9,17 +9,19 @@ class_name Sword extends Weapon
 ## time after swipe animation where action remains disabled before returning t idle state
 @export var cooldown_duration: float = 1.0
 
+var current_animation_tween: Tween
+
 func attack():
-	var animation_tween: Tween = get_tree().create_tween()
-	animation_tween.tween_property(self, 'rotation', self.rotation-1.1, 0.1)
-	await animation_tween.finished
+	current_animation_tween = get_tree().create_tween()
+	current_animation_tween.tween_property(self, 'rotation', self.rotation-1.1, 0.1)
+	await current_animation_tween.finished
 	
 	hitbox.disabled = false
 	sprite.play('active')
 	
-	animation_tween = get_tree().create_tween()
-	animation_tween.tween_property(self, 'rotation', self.rotation+2.1, swipe_duration)
-	await animation_tween.finished
+	current_animation_tween = get_tree().create_tween()
+	current_animation_tween.tween_property(self, 'rotation', self.rotation+2.1, swipe_duration)
+	await current_animation_tween.finished
 	
 	hitbox.disabled = true
 	sprite.play('inactive')
