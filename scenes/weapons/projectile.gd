d24a0b234166c283d624be9dc66316682f8e9b17
Andres Graciano <andres27l97@gmail.com>
2025-01-26 12:59:01 -0600

    scythe can melee/shoot now

--- scenes/weapons/projectile.gd
+++ scenes/weapons/projectile.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var Hitbox := $Hitbox
 var speed =10
 var direction= Vector2.RIGHT
 
@@ -12,4 +13,14 @@ func _process(delta):
 	
 	if collision:
 		queue_free()
+
+func enable_layer():
+	$Hitbox.set_collision_layer_value(4, true)
+	print($Hitbox.get_collision_layer_value(4))
+	print("it happened")
+	
+func disable_layer():
+	$Hitbox.set_collision_layer_value(4, false)
+	print($Hitbox.get_collision_layer_value(4))
+	print("wooo")
 	
