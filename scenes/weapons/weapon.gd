cb9ab6da7e8ea9d7510902a43d8e95828e0691e0
TheRealComedian <zacharylb3@gmail.com>
2025-01-28 21:34:04 -0500

    weapon animations saved and assigned propperly

--- scenes/weapons/weapon.gd
+++ scenes/weapons/weapon.gd
@@ -3,14 +3,6 @@ class_name Weapon extends Node2D
 @export var sprite: AnimatedSprite2D
 var user: Character
 
-func _ready():
-	if get_parent() is Character:
-		self.user = get_parent()
-	if self.user is Player: 
-		self.modulate = Color.YELLOW
-	if self.user is Enemy:
-		self.modulate = Color.RED
-
 ## Async method called in the attack script
 # override with any attack functionality like sprite/position changes or hitbox control
 func attack():
