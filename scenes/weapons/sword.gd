2cadbf52793d760c25e0cedf2cb53e0f318e6b60
Andres Graciano <andres27l97@gmail.com>
2025-01-29 23:18:57 -0600

    sword animation

--- scenes/weapons/sword.gd
+++ scenes/weapons/sword.gd
@@ -12,7 +12,7 @@ var hitbox_shape: CollisionShape2D
 @export var swipe_duration: float = 0.3
 ## time after swipe animation where action remains disabled before returning t idle state
 @export var cooldown_duration: float = 1.0
-
+@onready var animation_player := $AnimationPlayer
 var current_animation_tween: Tween
 var anim_duration = 0.6
 
@@ -24,17 +24,17 @@ func cancel_attack():
 func attack():
 	sprite.play('windup')
 	await Util.wait(self.anim_duration).timeout
-	current_animation_tween = get_tree().create_tween()
-	current_animation_tween.tween_property(self, 'rotation', self.rotation-1.1, 0.1)
-	await current_animation_tween.finished
+	#current_animation_tween = get_tree().create_tween()
+	#current_animation_tween.tween_property(self, 'rotation', self.rotation-1.1, 0.1)
+	#await current_animation_tween.finished
 	
-	hitbox_shape.disabled = false
+	#hitbox_shape.disabled = false
 	sprite.play('active')
+	animation_player.play("slash")
+	#current_animation_tween = get_tree().create_tween()
+	#current_animation_tween.tween_property(self, 'rotation', self.rotation+2.1, swipe_duration)
+	#await current_animation_tween.finished
 	
-	current_animation_tween = get_tree().create_tween()
-	current_animation_tween.tween_property(self, 'rotation', self.rotation+2.1, swipe_duration)
-	await current_animation_tween.finished
-	
-	hitbox_shape.disabled = true
+	#hitbox_shape.disabled = true
 	sprite.play('inactive')
-	await Util.wait(self.cooldown_duration).timeout
+	#await Util.wait(self.cooldown_duration).timeout
