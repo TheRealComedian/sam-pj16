4b1a8ef979b92731fa66a2026188ffb7ab40e92a
TheRealComedian <zacharylb3@gmail.com>
2025-01-30 13:25:43 -0500

    WIP will continue at work

--- scenes/world/map_exit.gd
+++ scenes/world/map_exit.gd
@@ -1,11 +1,11 @@
 class_name MapExit extends Area2D
 
 @export_category("Destination")
-@export_file('*.tscn') var map_path: String = "res://scenes/world/maps/"
+@export var destination_map: PackedScene
 @export var entry_point: String = "start"
 
 func _ready():
 	body_entered.connect(switch_map)
 
 func switch_map(body):
-	Map.set_as_current_map(load(map_path), entry_point)
+	Map.set_as_current_map(destination_map, entry_point)
