6f2e406aed1af5b00a38f88f172fc67051f57a03
TheRealComedian <zacharylb3@gmail.com>
2025-01-31 01:42:42 -0500

    maps working and connected

--- scenes/world/map.gd
+++ scenes/world/map.gd
@@ -26,12 +26,52 @@ func _exit_tree():
 	Global.camera = null
 	Global.map = null
 
+enum ROOM {
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN
+}
+
+const map_1 = preload('res://scenes/world/maps/room_1.tscn')
+const map_2 = preload('res://scenes/world/maps/room_2.tscn')
+const map_3 = preload('res://scenes/world/maps/room_3.tscn')
+const map_4 = preload('res://scenes/world/maps/room_4.tscn')
+const map_5 = preload('res://scenes/world/maps/room_5.tscn')
+const map_6 = preload('res://scenes/world/maps/room_6.tscn')
+const map_7 = preload('res://scenes/world/maps/room_7.tscn')
+const map_8 = preload('res://scenes/world/maps/room_8.tscn')
+const map_9 = preload('res://scenes/world/maps/room_9.tscn')
+const map_10 = preload('res://scenes/world/maps/room_10.tscn')
+
+static func load_room(room: ROOM) -> PackedScene:
+	match room:
+		ROOM.ONE	: return map_1
+		ROOM.TWO	: return map_2
+		ROOM.THREE	: return map_3
+		ROOM.FOUR	: return map_4
+		ROOM.FIVE	: return map_5
+		ROOM.SIX	: return map_6
+		ROOM.SEVEN	: return map_7
+		ROOM.EIGHT	: return map_8
+		ROOM.NINE	: return map_9
+		ROOM.TEN	: return map_10
+	return map_1
+
 static func set_as_current_map(map_resource: PackedScene, entry_point: String = 'start'):
+	
+	
 	# transition out
 	if Global.player:
 		Global.player.input_disabled = true
-	Global.transition.play('out')
-	await Global.transition.animation_finished
+	#Global.transition.play('out')
+	#await Global.transition.animation_finished
 	
 	
 
@@ -51,7 +91,7 @@ static func set_as_current_map(map_resource: PackedScene, entry_point: String = 
 	
 	# instance new map
 	var new_map: Map = map_resource.instantiate()
-	if new_map is not Map: 
+	if map_resource.instantiate() is not Map: 
 		push_error('map_resource "%s" does not unpack into a Map' % map_resource)
 		return
 	
@@ -66,6 +106,6 @@ static func set_as_current_map(map_resource: PackedScene, entry_point: String = 
 	Global.player.global_position = new_map.entry_points.get_node(entry_point).global_position
 	
 	# transition in
-	Global.transition.play('in')
-	await Global.transition.animation_finished
+	#Global.transition.play('in')
+	#await Global.transition.animation_finished
 	Global.player.input_disabled = false
--- scenes/world/map_exit.gd
+++ scenes/world/map_exit.gd
@@ -1,11 +1,11 @@
 class_name MapExit extends Area2D
 
 @export_category("Destination")
-@export var destination_map: PackedScene
+@export var destination_room: Map.ROOM
 @export var entry_point: String = "start"
 
 func _ready():
 	body_entered.connect(switch_map)
 
 func switch_map(body):
-	Map.set_as_current_map(destination_map, entry_point)
+	Map.set_as_current_map(Map.load_room(destination_room), entry_point)
